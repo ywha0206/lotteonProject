c2a4061017460a2d8e5af99a779db2244a3e9d3a
lsh9084 <sanghun1101088@gmail.com>
2024-10-16 17:40:26 +0900

    api마이컨트롤러 수정

---
+++ src/main/java/com/lotteon/controller/apicontroller/ApiMyPointController.java
@@ -0,0 +1,11 @@
+package com.lotteon.controller.apicontroller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/my")
+@RequiredArgsConstructor
+public class ApiMyPointController {
+}
