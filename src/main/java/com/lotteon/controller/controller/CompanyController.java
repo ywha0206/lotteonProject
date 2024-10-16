e59255a93cc853ff1afccb19a25e4e85fd90c17e
seungwoosin <soc23231@gmail.com>
2024-10-16 14:09:51 +0900

    컴퍼니 쪽 사진 및 추가

--- src/main/java/com/lotteon/controller/controller/CompanyController.java
+++ src/main/java/com/lotteon/controller/controller/CompanyController.java
@@ -1,9 +1,31 @@
 package com.lotteon.controller.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/company")
 public class CompanyController {
+
+    @GetMapping("/index")
+    public String index() {
+
+        return "pages/company/index";
+    }
+
+    @GetMapping("/culture")
+    public String culture() {
+        return "pages/company/culture";
+    }
+
+    @GetMapping("/recruit")
+    public String recruit() {
+        return "pages/company/recruit";
+    }
+
+    @GetMapping("/story")
+    public String story() {
+        return "pages/company/story";
+    }
 }
