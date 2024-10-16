608a6b255d28754406da0af49cb60f2b376d67af
lsh9084 <sanghun1101088@gmail.com>
2024-10-16 22:33:57 +0900

    config수정,주문관리 페이지생성

---
+++ src/main/java/com/lotteon/config/MyUserDetails.java
@@ -0,0 +1,76 @@
+package com.lotteon.config;
+
+import com.lotteon.entity.member.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class MyUserDetails implements UserDetails, OAuth2User{
+    private Member user;
+
+    private Map<String, Object> attributes;
+    private String accessToken;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return Map.of();
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+user.getMemRole())); // 계정 권한 앞에 접두어 ROLE_ 붙여야 됨
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getMemPwd();
+    }
+
+    @Override
+    public String getUsername() {
+        return String.valueOf(user.getId());
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return user.getMemUid();
+    }
+
+}
