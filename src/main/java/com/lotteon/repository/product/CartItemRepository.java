02b21797d290d9c498edb3b6b1e6a666a8ce2dbb
ywha0206 <ppyyhh3285@gmail.com>
2024-10-17 20:27:05 +0900

    cart

--- src/main/java/com/lotteon/repository/product/CartItemRepository.java
+++ src/main/java/com/lotteon/repository/product/CartItemRepository.java
@@ -1,12 +1,18 @@
 package com.lotteon.repository.product;
 
+import com.lotteon.entity.product.Cart;
 import com.lotteon.entity.product.CartItem;
+import com.lotteon.entity.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    Optional<CartItem> findByCartIdAndProductId(Long id, long prodId);
+//    List<CartItem> findByCartAndProductId(Cart cart, long prodId);
+
+
+    List<CartItem> findAllByCartAndProduct(Cart cart, Product prod);
 }
