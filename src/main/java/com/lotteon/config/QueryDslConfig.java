52aa1af3e39483943b4d5bcc538984559a0c9f59
Ju Gyeong <jptrnmrs@gmai.com>
2024-10-17 09:52:01 +0900

    ModelMapper

--- src/main/java/com/lotteon/config/QueryDslConfig.java
+++ src/main/java/com/lotteon/config/QueryDslConfig.java
@@ -3,6 +3,8 @@ package com.lotteon.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +17,16 @@ public class QueryDslConfig {
     @Bean
     public JPAQueryFactory queryFactory() {
         return new JPAQueryFactory(entityManager);
+    }
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true);
+
+        return modelMapper;
     }
 }
