6952adeb832f3fb0cc5aaf20a0f431659074550c
lsh9084 <sanghun1101088@gmail.com>
2024-10-17 11:21:38 +0900

    배포연습

--- src/main/java/com/lotteon/LotteonApplication.java
+++ src/main/java/com/lotteon/LotteonApplication.java
@@ -1,5 +1,6 @@
 package com.lotteon;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -7,6 +8,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class LotteonApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(LotteonApplication.class, args);
