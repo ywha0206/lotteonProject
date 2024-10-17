6f8a1b731937c305760bb2760687e335a0e74987
lsh9084 <sanghun1101088@gmail.com>
2024-10-18 01:19:37 +0900

    메인페이지 카테고리 출력 기능구현

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,44 +1,48 @@
 
 
 
+async function openCategory(event){
+    console.log(event.target.dataset.id)
+    let id = event.target.dataset.id
+    try {
+        const response = await fetch(`/categories?id=${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const result = await response.json();
+        console.log(result.cates)
+        const section = document.getElementById('data-response');
+        section.innerHTML = result.cates.map(v => {
+            // cates2에서 현재 카테고리의 이름에 해당하는 하위 카테고리를 찾기
+            const subCategories = result.cates2[v.name] || []; // 없으면 빈 배열로 기본값 설정
+            console.log(subCategories)
+            return `
+        <li class="dep2_line">
+            <a href="#" target="_blank" class="d2_tit cf">${v.name}
+                <ul class="dep3 cf">
+                    ${subCategories.map(sub => `
+                        <li><a href="#" target="_blank">${sub.name}</a></li>
+                    `).join('')}
+                </ul>
+            </a>
+        </li>
+    `;
+        }).join('');
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
 
 
-
-// // Depth1 클릭 시 하위 Depth2를 토글하는 함수
-// document.querySelectorAll('.dep1 > a > .dep2 > .flex > .dep2_line').forEach(function(el) {
-//     el.addEventListener('click', function(e) {
-//         e.preventDefault();  // 링크 클릭 방지
-//         var parentLi = this.parentElement;
-        
-//         // 다른 열려 있는 메뉴를 닫고, 클릭된 메뉴만 열리도록 처리
-//         document.querySelectorAll('.dep1 > a ').forEach(function(li) {
-//             if (li !== parentLi) {
-//                 li.classList.remove('active');
-//             }
-//         });
-        
-//         // 현재 클릭한 항목의 하위 메뉴 표시/숨김 토글
-//         parentLi.classList.toggle('active');
-//     });
-// });
-
-// // Depth2 클릭 시 Depth3를 토글하는 함수 (필요시)
-// document.querySelectorAll('.dep1 > a > .dep2').forEach(function(el) {
-//     el.addEventListener('click', function(e) {
-//         e.preventDefault();  // 링크 클릭 방지
-//         var parentLi = this.parentElement;
-        
-//         // Depth3 메뉴 토글
-//         parentLi.classList.toggle('active');
-//     });
-// });
-
-    
-
-function openCategory(){
-   
     document.querySelector('.dep2').style.display = 'block'
-    
+
 }
 
 function closePopupCategory(){
