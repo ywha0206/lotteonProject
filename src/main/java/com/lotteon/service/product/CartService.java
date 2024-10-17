e03b99babfaaa8d41df60e8a073681cdc703e6e6
ywha0206 <ppyyhh3285@gmail.com>
2024-10-17 17:13:06 +0900

    updating cart insert

--- src/main/java/com/lotteon/service/product/CartService.java
+++ src/main/java/com/lotteon/service/product/CartService.java
@@ -5,6 +5,7 @@ import com.lotteon.dto.requestDto.PostCartDto;
 import com.lotteon.entity.member.Customer;
 import com.lotteon.entity.product.Cart;
 import com.lotteon.entity.product.CartItem;
+import com.lotteon.entity.product.CartItemOption;
 import com.lotteon.entity.product.Product;
 import com.lotteon.repository.member.CustomerRepository;
 import com.lotteon.repository.product.CartItemOptionRepository;
@@ -12,9 +13,9 @@ import com.lotteon.repository.product.CartItemRepository;
 import com.lotteon.repository.product.CartRepository;
 import com.lotteon.repository.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -38,53 +40,70 @@ public class CartService {
 
     public ResponseEntity insertCart(PostCartDto postCartDto) {
 
-        MyUserDetails auth = (MyUserDetails) SecurityContextHolder.getContext()
-                                                                .getAuthentication()
-                                                                .getPrincipal();
+        try {
 
-        //유저 조회, 없으면 던져
-        long memId = Long.parseLong(auth.getUsername());
-        Customer customer = customerRepository.findById(memId)
-                .orElseThrow(() -> new RuntimeException("Customer not found"));
+//        MyUserDetails auth = (MyUserDetails) SecurityContextHolder.getContext()
+//                                                                .getAuthentication()
+//                                                                .getPrincipal();
+//
+//        //유저 조회, 없으면 던져
+//        long memId = Long.parseLong(auth.getUsername());
 
-        long custId = customer.getId();
-
-        //장바구니 조회 없으면 생성
-        Optional<Cart> optCart = cartRepository.findByCustId(custId);
-        Cart cart;
-        if (optCart.isPresent()) {
-            cart = optCart.get();
-        } else {
-            cart = Cart.builder().custId(custId).build();
-            cartRepository.save(cart);
-        }
-
-        //장바구니 아이템 있으면 수정 없으면 추가
-        CartItem cartItem;
-        Optional<CartItem> optCartItem = cartItemRepository.findByCartIdAndProductId(cart.getId(), postCartDto.getProdId());
-        if (optCartItem.isPresent()) {
-            cartItem = optCartItem.get();
-            int dtoQuan = postCartDto.getQuantity();
-            int entityQuan = cartItem.getQuantity();
-            int totalQuan = dtoQuan + entityQuan;
-
-            cartItem.setQuantity(totalQuan);
-        }else{
-            Product product = productRepository.findById(postCartDto.getProdId())
-                    .orElseThrow(() -> new RuntimeException("Product not found"));
-
-            cartItem = CartItem.builder()
-                    .cart(cart)
-                    .product(product)
-                    .quantity(postCartDto.getQuantity())
-                    .totalPrice(product.getProdPrice() * postCartDto.getQuantity())
-                    .build();
-            cartItemRepository.save(cartItem);
-        }
-
-
+            long memberId = 1;
+        Optional<Customer> optCustomer = customerRepository.findByMemberId(memberId);
+//                //.orElseThrow(() -> new RuntimeException("Customer not found"));
+            System.out.println(optCustomer.get());
+//            System.out.println(optCustomer.get());
+//            Customer customer = optCustomer.get();
+//            long custId = customer.getId();
+//
+//        //장바구니 조회 없으면 생성
+//        Optional<Cart> optCart = cartRepository.findByCustId(custId);
+//        Cart cart;
+//        if (optCart.isPresent()) {
+//            cart = optCart.get();
+//        } else {
+//            cart = Cart.builder().custId(custId).build();
+//            cartRepository.save(cart);
+//        }
+//
+//        //장바구니 아이템 있으면 수정 없으면 추가
+//        CartItem cartItem;
+//        Optional<CartItem> optCartItem = cartItemRepository.findByCartIdAndProductId(cart.getId(), postCartDto.getProdId());
+//        if (optCartItem.isPresent()) {
+//            cartItem = optCartItem.get();
+//            int dtoQuan = postCartDto.getQuantity();
+//            int entityQuan = cartItem.getQuantity();
+//            int totalQuan = dtoQuan + entityQuan;
+//
+//            cartItem.setQuantity(totalQuan);
+//        }else{
+//            Product product = productRepository.findById(postCartDto.getProdId())
+//                    .orElseThrow(() -> new RuntimeException("Product not found"));
+//
+//            cartItem = CartItem.builder()
+//                    .cart(cart)
+//                    .product(product)
+//                    .quantity(postCartDto.getQuantity())
+//                    .totalPrice(product.getProdPrice() * postCartDto.getQuantity())
+//                    .build();
+//            cartItemRepository.save(cartItem);
+//        }
+//
+//        //카트 아이템 옵션
+//        postCartDto.getOptions().forEach(option -> {
+//            CartItemOption cartItemOption = CartItemOption.builder()
+//                    .prodOptionId(option)
+//                    .cartItem(cartItem)
+//                    .build();
+//            cartItemOptionRepository.save(cartItemOption);
+//        } );
 
         return null;
+
+        }catch (Exception e) {
+            return null;
+        }
 
     }
 }
