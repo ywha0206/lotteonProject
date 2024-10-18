264f1b6743f51653c2d2940adade9913f75c4839
lsh9084 <sanghun1101088@gmail.com>
2024-10-18 13:08:03 +0900

    푸터 배포

--- src/main/java/com/lotteon/controller/controller/CommonController.java
+++ src/main/java/com/lotteon/controller/controller/CommonController.java
@@ -3,6 +3,7 @@ package com.lotteon.controller.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 public class CommonController {
@@ -12,6 +13,7 @@ public class CommonController {
     @Value(value = "${spring.application.name}")
     private String appName;
 
+    @ModelAttribute
     public void version(Model model) {
         model.addAttribute("version", version);
         model.addAttribute("appName", appName);
