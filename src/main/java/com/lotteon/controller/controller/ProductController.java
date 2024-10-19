61ca9bbefcc3c13f4998b5219abbaa6485d6f949
lsh9084 <sanghun1101088@gmail.com>
2024-10-20 01:09:26 +0900

    상품 사이드카테고리 목록 출력 레이아웃 수정

--- src/main/java/com/lotteon/controller/controller/ProductController.java
+++ src/main/java/com/lotteon/controller/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.lotteon.controller.controller;
 
+import com.lotteon.dto.responseDto.GetCategoryDto;
+import com.lotteon.service.category.CategoryProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
@@ -7,25 +9,38 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/prod")
 @RequiredArgsConstructor
 @Log4j2
 public class ProductController {
 
+    private final CategoryProductService categoryProductService;
+
     @GetMapping("/products")
     public String products(Model model) {
+        List<GetCategoryDto> category1 = categoryProductService.findCategory();
+
+        model.addAttribute("category1", category1);
         return "pages/product/list";
     }
 
     @GetMapping("/product")
     public String product(Model model) {
+        List<GetCategoryDto> category1 = categoryProductService.findCategory();
+
+        model.addAttribute("category1", category1);
         return "pages/product/view";
     }
 
 
     @GetMapping("/products/search")
     public String search(Model model) {
+        List<GetCategoryDto> category1 = categoryProductService.findCategory();
+
+        model.addAttribute("category1", category1);
         return "pages/product/search";
     }
 
