bd03f51a9937f94a74d2a830fe1fa1e8667a0cbe
lsh9084 <sanghun1101088@gmail.com>
2024-10-20 22:34:43 +0900

    관리자 테스트 판매자 계정 만들기 기능 구현

--- src/main/java/com/lotteon/service/member/SellerService.java
+++ src/main/java/com/lotteon/service/member/SellerService.java
@@ -1,5 +1,10 @@
 package com.lotteon.service.member;
 
+import com.lotteon.dto.requestDto.PostAdminSellerDto;
+import com.lotteon.entity.member.Member;
+import com.lotteon.entity.member.Seller;
+import com.lotteon.repository.member.MemberRepository;
+import com.lotteon.repository.member.SellerRepository;
 import com.lotteon.repository.term.TermsRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -17,5 +22,32 @@ public class SellerService {
     private final PasswordEncoder passwordEncoder;
     private final TermsRepository termsRepository;
     private final ModelMapper modelMapper;
+    private final MemberRepository memberRepository;
+    private final SellerRepository sellerRepository;
 
+    public void postSeller(PostAdminSellerDto postAdminSellerDto) {
+
+        Member member = Member.builder()
+                .memPwd(passwordEncoder.encode(postAdminSellerDto.getMemPwd()))
+                .memUid(postAdminSellerDto.getMemId())
+                .memRole("seller")
+                .memState("basic")
+                .build();
+
+        memberRepository.save(member);
+
+        Seller seller = Seller.builder()
+                .member(member)
+                .sellAddr(postAdminSellerDto.getSellAddr())
+                .sellCompany(postAdminSellerDto.getSellCompany())
+                .sellFax(postAdminSellerDto.getSellFax())
+                .sellBusinessCode(postAdminSellerDto.getSellCode())
+                .sellOrderCode(postAdminSellerDto.getSellOrderCode())
+                .sellRepresentative(postAdminSellerDto.getSellRep())
+                .sellHp(postAdminSellerDto.getSellHp())
+                .build();
+
+        sellerRepository.save(seller);
+
+    }
 }
