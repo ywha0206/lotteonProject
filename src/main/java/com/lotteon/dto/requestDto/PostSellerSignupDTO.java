4a2624a54633cf2ef03d8b22a9d2518941e85230
minhi0449 <minhi0449@gmail.com>
2024-10-21 17:06:44 +0900

    일반 회원가입 mem, cust 분리하여 DB 저장

--- src/main/java/com/lotteon/dto/requestDto/PostSellerSignupDTO.java
+++ src/main/java/com/lotteon/dto/requestDto/PostSellerSignupDTO.java
@@ -13,32 +13,18 @@ import lombok.*;
 @ToString
 public class PostSellerSignupDTO { // 판매회원 회원가입
 
-    private Long memId; // 판매자 ID
-
-    private String memPwd;
+    private Long   memId;               // 판매자 ID
+    private String memPwd;              // 판매자 PW
+    private int    sellGrade;           // 판매자 회원 등급
+    private String sellCompany;         // 회사명
+    private String sellRepresentative;  // 대표
+    private String sellBusinessCode;    // 사업자등록번호
+    private String sellOrderCode;       // 통신판매업번호
+    private String sellHp;              // 전화번호
+    private String sellFax;             // 팩스번호
+    private String sellAddr;            // 회사주소
 
 //    private String custName;
-//
 //    private String custGender;
-//
 //    private String custEmail;
-
-
-    private int sellGrade; // 판매자 회원 등급
-
-    private String sellCompany; // 회사명
-
-    private String sellRepresentative; // 대표
-
-    private String sellBusinessCode; // 사업자등록번호
-
-    private String sellOrderCode; // 통신판매업번호
-
-    private String sellHp; // 전화번호
-
-    private String sellFax; // 팩스번호
-
-    private String sellAddr; // 회사주소
-
-
 }
