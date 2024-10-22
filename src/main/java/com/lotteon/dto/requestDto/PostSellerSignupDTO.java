995b84d61029b000e31e33660f570f0f195c9045
minhi0449 <minhi0449@gmail.com>
2024-10-22 17:09:44 +0900

    회원가입 유효성 검사 기능구현 중

--- src/main/java/com/lotteon/dto/requestDto/PostSellerSignupDTO.java
+++ src/main/java/com/lotteon/dto/requestDto/PostSellerSignupDTO.java
@@ -10,11 +10,11 @@ import lombok.*;
 @ToString
 public class PostSellerSignupDTO { // 판매회원 회원가입
 
-    private String   memId;               // 판매자 ID
+    private String memId;               // 판매자 ID
     private String memPwd;              // 판매자 PW
     private int    sellGrade;           // 판매자 회원 등급
     private String sellCompany;         // 회사명
-    private String sellRepresentative;  // 대표
+    private String sellRepresentative;  // 대표명
     private String sellBusinessCode;    // 사업자등록번호
     private String sellOrderCode;       // 통신판매업번호
     private String sellHp;              // 전화번호
