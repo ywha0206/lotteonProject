b53ce8afac0e73c5c7190024bca27159a71ea810
ParkKyunglim <pkl1004125@gmail.com>
2024-10-22 17:07:19 +0900

    관리자 faq 글작작성 구현중

--- src/main/java/com/lotteon/controller/controller/AdminCsController.java
+++ src/main/java/com/lotteon/controller/controller/AdminCsController.java
@@ -1,11 +1,14 @@
 package com.lotteon.controller.controller;
 
+import com.lotteon.service.article.FaqService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/admin/cs")
@@ -16,12 +19,14 @@ public class AdminCsController {
     private String getSideValue() {
         return "cs";  // 실제 config 값을 여기에 설정합니다.
     }
+    private final FaqService faqService;
 
-    @GetMapping("/index")
+
+/*    @GetMapping("/index")
     public String index(Model model) {
         model.addAttribute("config", getSideValue());
         return "pages/admin/cs/index";
-    }
+    }*/
     @GetMapping("/faqs")
     public String faqs(Model model) {
         model.addAttribute("config", getSideValue());
@@ -69,6 +74,20 @@ public class AdminCsController {
     public String qnaWrite(Model model) {
         model.addAttribute("config", getSideValue());
         return "pages/admin/cs/qna/write";
+    }
+
+    // FAQ 작성
+    @PostMapping("/faq/write")
+    public String writeFaq(@RequestParam("faqCate") String category1,
+                           @RequestParam("faqType") String category2,
+                           @RequestParam("faqTitle") String title,
+                           @RequestParam("faqContent") String content) {
+        System.out.println("category1 = " + category1);
+        System.out.println("category2 = " + category2);
+        System.out.println("title = " + title);
+        System.out.println("content = " + content);
+        faqService.writeFaq(category1, category2, title, content);
+        return "redirect:/admin/cs/faqs";
     }
 
 }
