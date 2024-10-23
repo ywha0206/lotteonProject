140c72a81689ef4435a9c032828f56e35c89784e
minhi0449 <minhi0449@gmail.com>
2024-10-23 17:08:33 +0900

    1.일반 회원가입 유효성 검사 진행-> 중단 | 2. 관리자 회원목록 출력 구현중

--- src/main/java/com/lotteon/controller/controller/AdminUserController.java
+++ src/main/java/com/lotteon/controller/controller/AdminUserController.java
@@ -13,16 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Log4j2
 public class AdminUserController {
 
+
+
     private String getSideValue() {
         return "user";  // 실제 config 값을 여기에 설정합니다.
     }
 
+    // 1. 관리자 회원목록
     @GetMapping("/user")
     public String user(Model model) {
         model.addAttribute("config", getSideValue());
         return "pages/admin/user/user";
     }
 
+    // 2. 관리자 포인트관리 
     @GetMapping("/point")
     public String point(Model model) {
         model.addAttribute("config", getSideValue());
