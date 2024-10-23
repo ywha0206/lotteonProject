a804be98f3fb38c15f014d078656eaf9834a9c6b
lsh9084 <sanghun1101088@gmail.com>
2024-10-23 17:38:21 +0900

    이벤트 출석체크 구현/포인트적립 구현/ 포인트적립내역구현/

--- src/main/java/com/lotteon/service/point/PointService.java
+++ src/main/java/com/lotteon/service/point/PointService.java
@@ -1,6 +1,15 @@
 package com.lotteon.service.point;
 
+import com.lotteon.config.MyUserDetails;
+import com.lotteon.dto.responseDto.GetPointsDto;
+import com.lotteon.entity.member.Customer;
+import com.lotteon.entity.point.Point;
+import com.lotteon.repository.point.PointRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +17,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class PointService {
+    private final PointRepository pointRepository;
+
+    public Page<GetPointsDto> findAllByCustomer(int page) {
+        Pageable pageable = PageRequest.of(page, 5);
+        MyUserDetails auth = (MyUserDetails) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        Customer customer = auth.getUser().getCustomer();
+
+        Page<Point> points = pointRepository.findAllByCustId(customer.getId(),pageable);
+        Page<GetPointsDto> dtos = points.map(v->v.toGetPointsDto());
+
+        return dtos;
+    }
 }
