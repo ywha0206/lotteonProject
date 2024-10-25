e3429fd2065faada50c4145c4f83b1497f8e9e4c
lsh9084 <sanghun1101088@gmail.com>
2024-10-25 17:31:28 +0900

    관리자페이지 디자인 수정 및 생일이벤트

--- src/main/java/com/lotteon/controller/controller/MainController.java
+++ src/main/java/com/lotteon/controller/controller/MainController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,10 +17,11 @@ public class MainController {
     private final CategoryProductService categoryProductService;
 
     @GetMapping(value = {"/","/index"})
-    public String main(Model model) {
+    public String main(Model model, @RequestParam(value = "birth", defaultValue = "false") Boolean birth) {
         List<GetCategoryDto> category1 = categoryProductService.findCategory();
 
         model.addAttribute("category1", category1);
+        model.addAttribute("birth", birth);
         return "index";
     }
 }
