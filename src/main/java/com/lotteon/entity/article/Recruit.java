df9688fef2087c5a7f130d9421fe517b29b665dd
lsh9084 <sanghun1101088@gmail.com>
2024-10-25 23:17:18 +0900

    채용공고 등록/수정/글보기/목록/페이징/검색/ 회사소개 채용페이지 목록불러오기

---
+++ src/main/java/com/lotteon/entity/article/Recruit.java
@@ -0,0 +1,82 @@
+package com.lotteon.entity.article;
+
+import com.lotteon.dto.requestDto.PostRecruitDto;
+import com.lotteon.entity.member.Member;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.w3c.dom.Text;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@ToString
+@Entity
+public class Recruit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "career")
+    private String career;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "s_date")
+    private LocalDate sdate;
+
+    @Column(name = "e_date")
+    private LocalDate edate;
+
+    @Column(name = "etc",columnDefinition = "TEXT")
+    private String etc;
+
+    @Column(name = "r_date")
+    @CreationTimestamp
+    private LocalDateTime rdate;
+
+    @Column(name = "state")
+    private int state;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mem_id")
+    private Member member;
+
+    public PostRecruitDto toGetRecruitDto() {
+
+        return PostRecruitDto.builder()
+                .career(career)
+                .edate(String.valueOf(edate))
+                .etc(etc)
+                .title(title)
+                .type(type)
+                .rdate(rdate)
+                .sdate(String.valueOf(sdate))
+                .id(id)
+                .state(state)
+                .department(department)
+                .memUid(member.getMemUid())
+                .build();
+    }
+
+    public void updateRecruit(PostRecruitDto dto) {
+        this.edate = LocalDate.parse(dto.getEdate());
+        this.etc = dto.getEtc();
+        this.title = dto.getTitle();
+        this.type = dto.getType();
+        this.department = dto.getDepartment();
+        this.career = dto.getCareer();
+        this.sdate = LocalDate.parse(dto.getSdate());
+    }
+}
---
+++ src/main/java/com/lotteon/service/article/RecruitService.java
@@ -0,0 +1,109 @@
+package com.lotteon.service.article;
+
+import com.lotteon.config.MyUserDetails;
+import com.lotteon.dto.requestDto.PostRecruitDto;
+import com.lotteon.entity.article.Recruit;
+import com.lotteon.entity.member.Member;
+import com.lotteon.repository.article.RecruitRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RecruitService {
+
+    private final RecruitRepository recruitRepository;
+
+    public void insertRecruit(PostRecruitDto dto) {
+        Member member = this.getAuthentication();
+        Recruit recruit = Recruit.builder()
+                .etc(dto.getEtc())
+                .rdate(dto.getRdate())
+                .member(member)
+                .state(1)
+                .title(dto.getTitle())
+                .edate(LocalDate.parse(dto.getEdate()))
+                .sdate(LocalDate.parse(dto.getSdate()))
+                .career(dto.getCareer())
+                .type(dto.getType())
+                .department(dto.getDepartment())
+                .build();
+
+        recruitRepository.save(recruit);
+    }
+
+    private Member getAuthentication() {
+        MyUserDetails auth = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return auth.getUser();
+    }
+
+    public Page<PostRecruitDto> findAll(int page) {
+        Pageable pageable = PageRequest.of(page, 7);
+        Page<Recruit> recruits;
+
+        recruits = recruitRepository.findAllByOrderByEdateAscIdDesc(pageable);
+        Page<PostRecruitDto> dtos = recruits.map(Recruit::toGetRecruitDto);
+
+        return dtos;
+    }
+
+    public Page<PostRecruitDto> findAllBySearch(String searchType, String keyword, int page) {
+        Pageable pageable = PageRequest.of(page, 7);
+        Page<Recruit> recruits;
+
+        if(searchType.equals("id")){
+            recruits = this.findAllById(Long.parseLong(keyword),pageable);
+        } else if(searchType.equals("department")){
+            recruits = this.findAllByDepartment(keyword,pageable);
+        } else if (searchType.equals("type")) {
+            recruits = this.findAllByType(keyword,pageable);
+        } else {
+            recruits = this.findAllByState(keyword,pageable);
+
+        }
+        return recruits.map(Recruit::toGetRecruitDto);
+    }
+
+    private Page<Recruit> findAllByState(String keyword, Pageable pageable) {
+        Page<Recruit> recruits = recruitRepository.findAllByStateOrderByEdateAscIdDesc(Integer.parseInt(keyword),pageable);
+        return recruits;
+    }
+
+    private Page<Recruit> findAllByType(String keyword, Pageable pageable) {
+        Page<Recruit> recruits = recruitRepository.findAllByTypeOrderByEdateAscIdDesc(keyword,pageable);
+        return recruits;
+    }
+
+    private Page<Recruit> findAllByDepartment(String keyword, Pageable pageable) {
+        Page<Recruit> recruits = recruitRepository.findAllByDepartmentOrderByEdateAscIdDesc(keyword,pageable);
+        return recruits;
+    }
+
+    private Page<Recruit> findAllById(long id, Pageable pageable) {
+        Page<Recruit> recruits = recruitRepository.findAllByIdOrderByEdateAscIdDesc(id,pageable);
+        return recruits;
+    }
+
+    public PostRecruitDto findById(Long id) {
+        Optional<Recruit> recruit = recruitRepository.findById(id);
+        return recruit.get().toGetRecruitDto();
+    }
+
+    public void updateRecruit(PostRecruitDto dto) {
+        Optional<Recruit> recruit = recruitRepository.findById(dto.getId());
+        if(recruit.isEmpty()){
+            return;
+        }
+
+        recruit.get().updateRecruit(dto);
+        recruitRepository.save(recruit.get());
+    }
+}
