fbaac890960d3e0256826d235ed1c0f63857ea34
Ju Gyeong <jptrnmrs@gmail.com>
2024-10-27 20:02:10 +0900

    policy terms

--- src/main/java/com/lotteon/controller/controller/PolicyController.java
+++ src/main/java/com/lotteon/controller/controller/PolicyController.java
@@ -1,39 +1,41 @@
 package com.lotteon.controller.controller;
 
+import com.lotteon.dto.responseDto.GetTermsResponseDto;
+import com.lotteon.service.term.TermsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/policy")
 @RequiredArgsConstructor
 @Log4j2
 public class PolicyController {
-    @GetMapping("/customer")
-    public String customer(Model model) {
-        return "pages/policy/customer";
+
+    private final TermsService termsService;
+
+    @GetMapping("/{type}")
+    public String customer(@PathVariable("type") String type, Model model) {
+        log.info(type);
+        List<GetTermsResponseDto> list = termsService.selectAllTerms();
+        log.info(list);
+        int i = 0;
+        switch (type) {
+            case "seller" -> i=1;
+            case "finance" -> i=2;
+            case "location" -> i=3;
+            case "privacy" -> i=4;
+            default -> i=0;
+        }
+        model.addAttribute("type", i);
+        model.addAttribute("terms", list.get(i));
+        return "pages/policy/terms";
     }
 
-    @GetMapping("/seller")
-    public String seller(Model model) {
-        return "pages/policy/seller";
-    }
-
-    @GetMapping("/finance")
-    public String finance(Model model) {
-        return "pages/policy/finance";
-    }
-
-    @GetMapping("/location")
-    public String location(Model model) {
-        return "pages/policy/location";
-    }
-
-    @GetMapping("/privacy")
-    public String privacy(Model model) {
-        return "pages/policy/privacy";
-    }
 }
