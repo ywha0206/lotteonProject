f6fc1f30b3e2f0687ab598970da713f4a96559b9
Ju Gyeong <jptrnmrs@gmail.com>
2024-10-27 15:25:04 +0900

    config Redis Caching

--- src/main/java/com/lotteon/service/config/FLotteService.java
+++ src/main/java/com/lotteon/service/config/FLotteService.java
@@ -6,8 +6,11 @@ import com.lotteon.entity.config.FLotte;
 import com.lotteon.repository.config.FLotteRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 @RequiredArgsConstructor
@@ -16,12 +19,17 @@ public class FLotteService {
     private final ModelMapper modelMapper;
     private final FLotteRepository fLotteRepository;
 
+    @CacheEvict(value = "footerCache", key = "'fLotte'")
     public FLotte updateFooter(PostFLotteDTO footerDTO) {
         FLotte fLotte = modelMapper.map(footerDTO, FLotte.class);
         return fLotteRepository.save(fLotte);
     }
+    @Cacheable(value = "footerCache", key = "'fLotte'", cacheManager = "cacheManager")
     public GetFLotteDTO getRecentFLotte () {
+        System.out.println("디비 접속 후 Footer 정보 조회");
         FLotte fLotte = fLotteRepository.findTopByOrderByIdDesc();
         return modelMapper.map(fLotte, GetFLotteDTO.class);
     }
+
+
 }
