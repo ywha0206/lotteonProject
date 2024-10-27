f2f73121859715aace63262e5e14f529262db0e2
Ju Gyeong <jptrnmrs@gmail.com>
2024-10-27 15:25:04 +0900

    config Redis Caching

--- src/main/java/com/lotteon/service/config/FCsService.java
+++ src/main/java/com/lotteon/service/config/FCsService.java
@@ -7,6 +7,8 @@ import com.lotteon.entity.config.FCs;
 import com.lotteon.repository.config.FCsRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,11 +19,13 @@ public class FCsService {
     private final FCsRepository fCsRepository;
     private final ModelMapper modelMapper;
 
+    @CacheEvict(value = "footerCache", key = "'fCs'")
     public FCs updateCS(PostFCsDTO csDTO) {
         FCs fCs = modelMapper.map(csDTO, FCs.class);
         return fCsRepository.save(fCs);
     }
 
+    @Cacheable(value = "footerCache", key = "'fCs'", cacheManager = "cacheManager")
     public GetFCsDTO getRecentFCs() {
         FCs fCs = fCsRepository.findTopByOrderByIdDesc();
         return modelMapper.map(fCs, GetFCsDTO.class);
