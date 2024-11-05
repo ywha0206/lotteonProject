92881247295560ed022c722c0b8e1d9caf1bd43d
Ju Gyeong <jptrnmrs@gmai.com>
2024-11-05 12:43:09 +0900

    배너 출력

--- src/main/java/com/lotteon/controller/controller/AuthController.java
+++ src/main/java/com/lotteon/controller/controller/AuthController.java
@@ -25,6 +25,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Random;
 
 @Log4j2
 @Controller
@@ -39,8 +40,14 @@ public class AuthController {
 
     @GetMapping("/login/view")
     public String login(Model model) {
-        List<GetBannerDTO> bannerList = bannerService.selectUsingBannerAt(4);
-        model.addAttribute("banner", bannerList);
+        List<GetBannerDTO> banners = bannerService.selectUsingBannerAt(4);
+        if(banners.size()>1){
+            Random random = new Random();
+            int randomIndex = random.nextInt(banners.size());
+            model.addAttribute("banner", banners.get(randomIndex));
+        }else{
+            model.addAttribute("banner", banners.get(0));
+        }
         return "pages/auth/login";
     }
 
