5901716600988661eb1086c6e99465b486b93d21
ywha0206 <ppyyhh3285@gmail.com>
2024-11-06 16:01:48 +0900

    my order 상세정보

--- src/main/java/com/lotteon/controller/apicontroller/ApiMyOrderController.java
+++ src/main/java/com/lotteon/controller/apicontroller/ApiMyOrderController.java
@@ -9,12 +9,10 @@ import com.lotteon.service.product.OrderItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
-
-@Log4j2
-
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Log4j2
 @RestController
 @RequestMapping("/my")
 @RequiredArgsConstructor
