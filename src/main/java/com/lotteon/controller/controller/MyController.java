37f9d9b5d16473ca252941c93c2d668c53a1949e
Ju Gyeong <jptrnmrs@gmail.com>
2024-11-07 09:12:04 +0900

    Update MyController.java

--- src/main/java/com/lotteon/controller/controller/MyController.java
+++ src/main/java/com/lotteon/controller/controller/MyController.java
@@ -232,14 +232,14 @@ public class MyController {
         return "pages/my/usepoint";
     }
 
-    @GetMapping("/qnas")
+    /* @GetMapping("/qnas")
     public String getMyQnas(Model model, Principal principal) {
         MyUserDetails userDetails = (MyUserDetails) ((Authentication) principal).getPrincipal();
         Member memberId = userDetails.getUser(); // MyUserDetails에서 ID를 가져오는 메서드 사용
         List<ArticleDto> qnaList = qnaService.getMyQnas(memberId.getId());
         model.addAttribute("qnaList", qnaList);
         return "pages/my/qna"; // 뷰 파일로 연결
-    }
+    }*/
 
     @GetMapping("/qnas")
     public String getMyQnas(Model model, Principal principal,
