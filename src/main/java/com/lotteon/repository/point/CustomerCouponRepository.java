61a08ce9188d00acb533d7dd316465de6cd468df
lsh9084 <sanghun1101088@gmail.com>
2024-11-12 22:25:51 +0900

    각종 오류수정

--- src/main/java/com/lotteon/repository/point/CustomerCouponRepository.java
+++ src/main/java/com/lotteon/repository/point/CustomerCouponRepository.java
@@ -16,36 +16,17 @@ import java.util.Optional;
 public interface CustomerCouponRepository extends JpaRepository<CustomerCoupon, Long> {
     Optional<CustomerCoupon> findByCustomerAndCoupon(Customer customer, Coupon coupon);
 
-    List<CustomerCoupon> findAllByCoupon(Coupon coupon);
-
-//    Page<CustomerCoupon> findAllByOrderByCouponStateAscCouponUDateDesc(Pageable pageable);
-
-    Page<CustomerCoupon> findAllByIdOrderByCouponStateAscCouponUDateDesc(Long id, Pageable pageable);
-
     Page<CustomerCoupon> findAllByCouponOrderByCouponStateAscIdDesc(Coupon coupon, Pageable pageable);
 
     Page<CustomerCoupon> findAllByCustomer(Customer customer, Pageable pageable);
-
-    Page<CustomerCoupon> findAllByCouponAndIdOrderByCouponStateAscIdDesc(Coupon coupon, long l, Pageable pageable);
-
-    Page<CustomerCoupon> findAllByCouponAndCouponIdOrderByCouponStateAscIdDesc(Coupon coupon, long l, Pageable pageable);
 
     Page<CustomerCoupon> findAllByCouponAndCustomerOrderByCouponStateAscIdDesc(Coupon coupon, Customer customer, Pageable pageable);
 
     int countCustomerCouponsByCustomer(Customer customer);
 
-
     Page<CustomerCoupon> findAllByOrderByCouponStateAscIdDesc(Pageable pageable);
 
-    Page<CustomerCoupon> findAllByIdOrderByCouponStateAscIdDesc(long l, Pageable pageable);
-
     List<CustomerCoupon> findAllByCouponState(int i);
-
-    List<CustomerCoupon> findAllByCustomerAndCoupon_Member(Customer customer, Member seller);
-
-    List<CustomerCoupon> findAllByCustomer(Customer customer);
-
-    List<CustomerCoupon> findAllByCustomerAndCoupon_Member_MemRole(Customer customer, String admin);
 
     List<CustomerCoupon> findAllByCustomerAndCoupon_MemberAndCouponState(Customer customer, Member member, int i);
 
@@ -54,4 +35,14 @@ public interface CustomerCouponRepository extends JpaRepository<CustomerCoupon, 
     List<CustomerCoupon> findAllByCustomerAndCouponState(Customer customer, int i);
 
     Page<CustomerCoupon> findAllByCustomerAndCouponState(Customer customer, int i, Pageable pageable);
+
+    Page<CustomerCoupon> findAllByOrderByIdDesc(Pageable pageable);
+
+    Page<CustomerCoupon> findAllByCouponOrderByIdDesc(Coupon coupon, Pageable pageable);
+
+    Page<CustomerCoupon> findAllByCouponAndIdOrderByIdDesc(Coupon coupon, long l, Pageable pageable);
+
+    Page<CustomerCoupon> findAllByCouponAndCouponIdOrderByIdDesc(Coupon coupon, long l, Pageable pageable);
+
+    Page<CustomerCoupon> findAllByIdOrderByIdDesc(long l, Pageable pageable);
 }
