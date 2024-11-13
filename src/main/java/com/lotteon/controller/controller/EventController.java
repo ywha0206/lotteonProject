3dce477369ebe60fc22a71910d2fd78ad4c97b82
lsh9084 <sanghun1101088@gmail.com>
2024-11-13 12:22:07 +0900

    출석이벤트 오류수정

--- src/main/java/com/lotteon/controller/controller/EventController.java
+++ src/main/java/com/lotteon/controller/controller/EventController.java
@@ -4,6 +4,7 @@ import com.lotteon.config.MyUserDetails;
 import com.lotteon.entity.member.AttendanceEvent;
 import com.lotteon.entity.member.Customer;
 import com.lotteon.repository.member.AttendanceEventRepository;
+import com.lotteon.repository.member.CustomerRepository;
 import com.lotteon.service.event.EventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/event")
 @Controller
@@ -21,22 +23,25 @@ public class EventController {
 
     private final EventService eventService;
     private final AttendanceEventRepository attendanceEventRepository;
+    private final CustomerRepository customerRepository;
 
     @GetMapping("/attendance")
-    public String attendance(Model model) {
-        String result = eventService.updateEvent();
-        MyUserDetails auth = (MyUserDetails) SecurityContextHolder.getContext()
-                .getAuthentication()
-                .getPrincipal();
-        Customer customer = auth.getUser().getCustomer();
-        AttendanceEvent event = attendanceEventRepository.findByCustomer(customer);
+    public String attendance(Model model, @RequestParam Long id) {
+        System.out.println(id);
+        String result = eventService.updateEvent(id);
+        System.out.println("==================");
+        System.out.println(result);
+        Customer customer = customerRepository.findById(id).get();
+        AttendanceEvent event = attendanceEventRepository.findByCustomer(customer).get();
         if(event.getAttendanceMiddleState()==1){
             model.addAttribute("middle",true);
         }
         if(event.getAttendanceState()==1){
             model.addAttribute("state",true);
         }
-        int days = eventService.findEvent(result);
+        int days = eventService.findEvent(result,id);
+        System.out.println("==================");
+        System.out.println(days);
         model.addAttribute("days", days);
         return "pages/event/attendance";
     }
